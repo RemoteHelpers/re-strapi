d051f3275827870ba996fe928f9f44074d668b78
Sofia Protsiv <prochivsofia@gmail.com>
2022-10-11 15:13:09 +0300

    Update index.js

--- src/pages/notFoundPage/index.js
+++ src/pages/notFoundPage/index.js
@@ -1,4 +1,8 @@
 /* eslint-disable @typescript-eslint/quotes */
+<<<<<<< Updated upstream
 // import { NotFoundPage } from "./NotFoundPage";
 
 // export default NotFoundPage;
+=======
+export { default } from "./NotFoundPage";
+>>>>>>> Stashed changes
