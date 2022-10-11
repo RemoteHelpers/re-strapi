6cba45d2a665ed60827b9790bb694f02ba4ba26f
Sofia Protsiv <prochivsofia@gmail.com>
2022-10-11 15:14:12 +0300

    fix: resolve conflict

--- src/pages/notFoundPage/index.js
+++ src/pages/notFoundPage/index.js
@@ -1,8 +1,2 @@
 /* eslint-disable @typescript-eslint/quotes */
-<<<<<<< Updated upstream
-// import { NotFoundPage } from "./NotFoundPage";
-
-// export default NotFoundPage;
-=======
 export { default } from "./NotFoundPage";
->>>>>>> Stashed changes
