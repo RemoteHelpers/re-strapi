7500deda2b1bcee9f4ebd2063acf0a7eed48d2cd
Andrew <mysecondmail0099@gmail.com>
2022-10-20 12:32:37 +0300

    andrew fix bugs

--- src/components/toTopButton/ToTopButton.tsx
+++ src/components/toTopButton/ToTopButton.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { FormsSvg } from '../../icons/form/FormsSvg';
 import cl from './toTopButton.module.scss';
@@ -8,6 +9,7 @@ export const ToTopButton = () => {
       top: 0,
       behavior: 'smooth',
     });
+    console.log('click');
   };
 
   return (
@@ -16,7 +18,7 @@ export const ToTopButton = () => {
       className={cl.to_top_btn}
       onClick={scrollTop}
     >
-      <FormsSvg id="top">Scroll Top</FormsSvg>
+      <FormsSvg id="top" />
     </button>
   );
 };
