70b6aef720072293114de0d74dce80b0bf3b8471
Sofia Protsiv <prochivsofia@gmail.com>
2022-11-08 17:22:04 +0200

    add dynamic param :lng

--- src/components/vacancyCard/VacancyCard.tsx
+++ src/components/vacancyCard/VacancyCard.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./vacancyCard.scss";
 import FireIcon from "../../images/fireIcon.svg";
 import { useStateContext } from "../../context/StateContext";
@@ -13,41 +13,42 @@ interface Props {
 }
 
 const VacancyCard: React.FC<Props> = ({ title, slug, isHot }) => {
-  const { setCurrentVacancy } = useStateContext();
+  const { setCurrentVacancy, localization } = useStateContext();
   const handleSlug = () => {
     setCurrentVacancy(slug);
   };
 
   return (
-    <div className="VacancyCard">
-      <div className="VacancyCard__banner VacancyCard__banner--mobile">
-        <img src={FireIcon} className="VacancyCard__fireIcon" alt="icon" />
-        <span className="VacancyCard__banner-text">
-          {isHot ? "Urgent" : "Not urgent"}
-        </span>
-      </div>
+    <Link to={`/${localization}/vacancies/${slug}`} onClick={handleSlug}>
+      <div className="VacancyCard">
+        <div className="VacancyCard__banner VacancyCard__banner--mobile">
+          <img src={FireIcon} className="VacancyCard__fireIcon" alt="icon" />
+          <span className="VacancyCard__banner-text">
+            {isHot ? "Urgent" : "Not urgent"}
+          </span>
+        </div>
 
-      <div className="VacancyCard__info">
-        <h3 className="VacancyCard__title">{title}</h3>
-        <p className="VacancyCard__salary">Salary based on interview results</p>
-        <p className="VacancyCard__description">
-          Our company is in search of a lead generator who will search for new
-          customers and information on the Internet, work with social networks.
-        </p>
-      </div>
-      <Link
-        className="VacancyCard__button"
-        to={`/vacancies/${slug}`}
-        onClick={handleSlug}
-      >
-        See more
-      </Link>
+        <div className="VacancyCard__info">
+          <h3 className="VacancyCard__title">{title}</h3>
+          <p className="VacancyCard__salary">
+            Salary based on interview results
+          </p>
+          <p className="VacancyCard__description">
+            Our company is in search of a lead generator who will search for new
+            customers and information on the Internet, work with social
+            networks.
+          </p>
+        </div>
+        <button type="button" className="VacancyCard__button">
+          See more
+        </button>
 
-      <div className="VacancyCard__banner VacancyCard__banner--desktop">
-        <img src={FireIcon} className="VacancyCard__fireIcon" alt="icon" />
-        <span className="VacancyCard__banner-text">Urgent</span>
+        <div className="VacancyCard__banner VacancyCard__banner--desktop">
+          <img src={FireIcon} className="VacancyCard__fireIcon" alt="icon" />
+          <span className="VacancyCard__banner-text">Urgent</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable object-curly-newline */
 /* eslint-disable comma-dangle */
 /* eslint-disable @typescript-eslint/quotes */
